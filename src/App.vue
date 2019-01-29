d9ef67db9725e720e0a20dd4a04bed8558b1c97d
huiyuanjian <yuanjian.hui@sinnet-cloud.cn>
2019-01-29 16:29:48 +0800

    初次提交

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <div id="nav">
+
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+* {
+  padding: 0;
+  margin: 0;
+}
+</style>
---
+++ src/views/User.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>user
+      <div class="btn" @click="add()">添加</div>
+    </div>
+</template>
+
+<script>
+	export default {
+		name: 'User',
+    methods: {
+      add () {
+        this.$router.push('/add')
+      }
+    }
+	}
+</script>
+
+<style scoped>
+
+</style>
